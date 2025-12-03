78ac8056b981eecb1905c75b943ccc9771c762b4
Dragon-hab <solomnijo@gmail.com>
2025-12-03 22:42:49 +0200

    Delete backend/app directory

--- backend/app/api/api_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .endpoints import auth, events, registrations, feedback
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix='/auth', tags=['auth'])
-api_router.include_router(events.router, prefix='/events', tags=['events'])
-api_router.include_router(registrations.router, prefix='/registrations', tags=['registrations'])
-api_router.include_router(feedback.router, prefix='/feedback', tags=['feedback'])
